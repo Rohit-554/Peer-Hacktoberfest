02132bf90acbd9f1f5423910765985f4fd52992e
sakshigjadhav-03 <sakshigjadhav03@gmail.com>
2025-10-15 16:30:15 +0530

    Added project in java

---
+++ src/miniproject.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.time.LocalDate;
+
+class Expense {
+    private String category;
+    private double amount;
+    private LocalDate date;
+
+    public Expense(String category, double amount, LocalDate date) {
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getCategory() { return category; }
+    public double getAmount() { return amount; }
+    public LocalDate getDate() { return date; }
+
+    @Override
+    public String toString() {
+        return "Category: " + category + " | Amount: $" + amount + " | Date: "
